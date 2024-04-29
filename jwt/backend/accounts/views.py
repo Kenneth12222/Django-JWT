58a6dc9f653f8e76e258aa505bc8ee95ed9daeda
Kenneth Mburu <kenneth.mburu@student.moringaschool.com>
2024-04-29 08:12:09 +0300

    view component, registration function, it handle registration func...

--- jwt/backend/accounts/views.py
+++ jwt/backend/accounts/views.py
@@ -5,3 +5,17 @@ from .serializers import *
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
+
+class UserRegistrationAPIView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegistrationSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["tokens"] = {"refresh":str(token),
+                          "access": str(token.access_token)}
+        return Response(data, status= status.HTTP_201_CREATED)
